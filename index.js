bafbcae8cfd0d07d677e63a5c6448b6898bea0fd
manojachar <manojy0505@gmail.com>
2019-07-26 11:36:53 +0530

    Prepared to deploy 1

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const connectDB = require("./config/db");
-const path = require("path");
+
 const cors = require("cors");
 connectDB();
 
